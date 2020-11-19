87446b1a4c040cd0cd5d2aae06de05a1ddfe4363
Javier Andres Pedrozo Payares <7519384+pedrozopayares@users.noreply.github.com>
2020-11-19 17:41:07 -0500

    Function to play sound added.

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,69 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = new AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: FlatButton(
-              child: Text('Press me!'),
-              onPressed: () {
-                final player = new AudioCache();
-                player.play('note1.wav');
-              },
+            child: Column(
+              children: [
+                FlatButton(
+                  child: SizedBox(),
+                  color: Colors.red,
+                  onPressed: () {
+                    playSound(1);
+                  },
+                ),
+                FlatButton(
+                  child: SizedBox(),
+                  color: Colors.orange,
+                  onPressed: () {
+                    playSound(2);
+                  },
+                ),
+                FlatButton(
+                  child: SizedBox(),
+                  color: Colors.yellow,
+                  onPressed: () {
+                    playSound(3);
+                  },
+                ),
+                FlatButton(
+                  child: SizedBox(),
+                  color: Colors.green,
+                  onPressed: () {
+                    playSound(4);
+                  },
+                ),
+                FlatButton(
+                  child: SizedBox(),
+                  color: Colors.teal[900],
+                  onPressed: () {
+                    playSound(5);
+                  },
+                ),
+                FlatButton(
+                  child: SizedBox(),
+                  color: Colors.blue,
+                  onPressed: () {
+                    playSound(6);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.cyan,
+                  child: SizedBox(),
+                  onPressed: () {
+                    playSound(7);
+                  },
+                ),
+              ],
             ),
           ),
         ),
